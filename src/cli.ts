a1d5001768b5ae16b770504ffd5f4c29f784ea1e
Cyril Schumacher <cyril.schumacher@arhs-developments.com>
2016-06-19 22:49:46 +0200

    Add cli support.

---
+++ src/cli.ts
@@ -0,0 +1,93 @@
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Cyril Schumacher.fr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/// <reference path="../typings/index.d.ts"/>
+
+import * as program from "commander";
+import * as fs from "fs";
+import {JsonPropertyFilter} from "./jsonPropertyFilter";
+
+program
+    .version('1.1.2')
+    .description('Filter a JSON file by including or excluding properties.')
+    .usage('[file]')
+    .option('-f, --filters [filters]', 'Include and exclude filters.')
+    .option('-o, --out [file]', 'Place output in file file.')
+    .option('-p, --pretty', 'Pretty output.')
+    .action(_apply)
+    .parse(process.argv);
+
+function _apply(file, options) {
+    fs.readFile(file, (error, data) => {
+        _assertReadFile(file, error);
+
+        const jsonObject = _transformToJsonObject(data);
+        const filteredJsonObject = _filterJsonObject(jsonObject, options);
+        _out(filteredJsonObject, options.out, options.pretty);
+    });
+}
+
+function _assertReadFile(file, error) {
+    if (error) {
+        throw new Error(`An error occurred while the reading of file: ${file}`);
+    }
+}
+
+function _assertWriteFile(error) {
+    if (error) {
+        throw new Error("An error occurred while saving of filtered JSON object.");
+    }
+}
+
+function _filterJsonObject(jsonObject, options) {
+    const jsonPropertyFilter = new JsonPropertyFilter(options.filters);
+    return jsonPropertyFilter.apply(jsonObject);
+}
+
+function _out(filteredJsonObject, outputFile, pretty) {
+    const jsonObject = pretty ? JSON.stringify(filteredJsonObject, null, 2) : JSON.stringify(filteredJsonObject);
+
+    if (outputFile) {
+        _writeResult(outputFile, jsonObject);
+    } else {
+        console.log(jsonObject);
+    }
+}
+
+function _transformToJsonObject(data) {
+    const fileContent = data.toString();
+
+    try {
+        return JSON.parse(fileContent);
+    } catch (e) {
+        throw new Error("An error occurred while processing of file content in a JSON object.")
+    }
+}
+
+function _writeOutputFile(error) {
+    _assertWriteFile(error);
+}
+
+function _writeResult(outputFile, data) {
+    fs.writeFile(outputFile, data, _writeOutputFile);
+}
