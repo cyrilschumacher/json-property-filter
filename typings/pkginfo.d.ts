fb69271523213285002fe63289cdb59b2d96fb1d
Cyril Schumacher <cyril_schumacher@live.fr>
2017-09-27 19:31:22 +0200

    Add missing typings.
    Fix "test:coverage" command.

---
+++ typings/pkginfo.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for figlet 1.2
+// Project: https://github.com/patorjk/figlet.js
+// Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module "pkginfo" {
+    function pkginfo(module: NodeModule, ...properties: string[]): any;
+    export = pkginfo;
+}
