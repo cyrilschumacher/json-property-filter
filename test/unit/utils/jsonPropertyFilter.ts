56455c3fa20d985a1f7544e7213aa6ae72747f06
Cyril Schumacher <cyril_schumacher@live.fr>
2017-09-27 21:39:44 +0200

    Update .npmignore
    Update file headers.

--- test/unit/utils/jsonPropertyFilter.ts
+++ test/unit/utils/jsonPropertyFilter.ts
@@ -1,6 +1,6 @@
 /* The MIT License (MIT)
  *
- * Copyright (c) 2016 Cyril Schumacher.fr
+ * Copyright (c) 2017 Cyril Schumacher.fr
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +20,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-/// <reference types="mocha" />
 
 import { assert } from "chai";
 import { JsonPropertyFilter } from "../../../src/jsonPropertyFilter";
